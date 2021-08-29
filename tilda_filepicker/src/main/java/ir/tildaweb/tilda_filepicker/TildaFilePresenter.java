644976b0c1beb79ffb7167d05c1ddee66f929e17
Amjad Qasemie Rad <amjadrad@gmail.com>
2021-08-30 01:42:57 +0430

    Fix problem: CursorIndexOutOfBoundsException.

--- tilda_filepicker/src/main/java/ir/tildaweb/tilda_filepicker/TildaFilePresenter.java
+++ tilda_filepicker/src/main/java/ir/tildaweb/tilda_filepicker/TildaFilePresenter.java
@@ -30,14 +30,15 @@ public class TildaFilePresenter {
         List<FileModel> list = new ArrayList<>();
         Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
         assert cursor != null;
-        cursor.moveToLast();
-        do {
-            String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
-            FileModel model = new FileModel();
-            model.setPath(absolutePath);
-            model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_IMAGE);
-            list.add(model);
-        } while (cursor.moveToPrevious());
+        if (cursor.moveToLast()) {
+            do {
+                String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                FileModel model = new FileModel();
+                model.setPath(absolutePath);
+                model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_IMAGE);
+                list.add(model);
+            } while (cursor.moveToPrevious());
+        }
         cursor.close();
         onDataPreparedListener.onResponseImages(list);
     }
@@ -56,46 +57,47 @@ public class TildaFilePresenter {
 
         Cursor cursor = context.getContentResolver().query(MediaStore.Files.getContentUri("external"), projection, selectionMimeType, selectionArgsPdf, null);
         assert cursor != null;
-        cursor.moveToLast();
-        do {
-            String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
-            FileModel model = new FileModel();
-            model.setPath(absolutePath);
-            model.setTitle(absolutePath.substring(absolutePath.lastIndexOf('/') + 1));
-            String mimeType = absolutePath.substring(absolutePath.lastIndexOf('.') + 1);
-            switch (mimeType) {
-                case "pdf": {
-                    model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_PDF);
-                    break;
-                }
-                case "zip": {
-                    model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_ZIP);
-                    break;
-                }
-                case "rar": {
-                    model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_RAR);
-                    break;
-                }
-                case "txt": {
-                    model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_TEXT);
-                    break;
-                }
-                case "html": {
-                    model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_HTML);
-                    break;
-                }
-                case "docx": {
-                    model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_DOCX);
-                    break;
-                }
-                default: {
-                    model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_NONE);
-                }
+        if (cursor.moveToLast()) {
+            do {
+                String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                FileModel model = new FileModel();
+                model.setPath(absolutePath);
+                model.setTitle(absolutePath.substring(absolutePath.lastIndexOf('/') + 1));
+                String mimeType = absolutePath.substring(absolutePath.lastIndexOf('.') + 1);
+                switch (mimeType) {
+                    case "pdf": {
+                        model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_PDF);
+                        break;
+                    }
+                    case "zip": {
+                        model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_ZIP);
+                        break;
+                    }
+                    case "rar": {
+                        model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_RAR);
+                        break;
+                    }
+                    case "txt": {
+                        model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_TEXT);
+                        break;
+                    }
+                    case "html": {
+                        model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_HTML);
+                        break;
+                    }
+                    case "docx": {
+                        model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_DOCX);
+                        break;
+                    }
+                    default: {
+                        model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_NONE);
+                    }
 
-            }
-            list.add(model);
-            Log.d(TAG, "requestFiles: " + model.getFileMimeType().name() + " :::::: " + absolutePath);
-        } while (cursor.moveToPrevious());
+                }
+                list.add(model);
+                Log.d(TAG, "requestFiles: " + model.getFileMimeType().name() + " :::::: " + absolutePath);
+            } while (cursor.moveToPrevious());
+        }
         cursor.close();
         onDataPreparedListener.onResponseFiles(list);
     }
@@ -104,15 +106,16 @@ public class TildaFilePresenter {
         List<FileModel> list = new ArrayList<>();
         Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
         assert cursor != null;
-        cursor.moveToLast();
-        do {
-            String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
-            FileModel model = new FileModel();
-            model.setTitle(absolutePath.substring(absolutePath.lastIndexOf('/') + 1));
-            model.setPath(absolutePath);
-            model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_MUSIC);
-            list.add(model);
-        } while (cursor.moveToPrevious());
+        if (cursor.moveToLast()) {
+            do {
+                String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                FileModel model = new FileModel();
+                model.setTitle(absolutePath.substring(absolutePath.lastIndexOf('/') + 1));
+                model.setPath(absolutePath);
+                model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_MUSIC);
+                list.add(model);
+            } while (cursor.moveToPrevious());
+        }
         cursor.close();
         onDataPreparedListener.onResponseMusics(list);
     }
@@ -121,14 +124,15 @@ public class TildaFilePresenter {
         List<FileModel> list = new ArrayList<>();
         Cursor cursor = context.getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
         assert cursor != null;
-        cursor.moveToLast();
-        do {
-            String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
-            FileModel model = new FileModel();
-            model.setPath(absolutePath);
-            model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_VIDEO);
-            list.add(model);
-        } while (cursor.moveToPrevious());
+        if (cursor.moveToLast()) {
+            do {
+                String absolutePath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                FileModel model = new FileModel();
+                model.setPath(absolutePath);
+                model.setFileMimeType(FileMimeType.FILE_MIME_TYPE_VIDEO);
+                list.add(model);
+            } while (cursor.moveToPrevious());
+        }
         cursor.close();
         onDataPreparedListener.onResponseVideos(list);
     }
